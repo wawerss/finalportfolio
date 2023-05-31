dd3dc18b11e22ea10390732115d39b4740b11eee
wawerss <134521619+wawerss@users.noreply.github.com>
2023-06-01 01:47:54 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,35 +1,21 @@
 $(document).ready(function(){
   $('.slick-carousel').slick({
-    dots: false,
-    rows: 3,
-    slidesPerRow: 1,
-    prevArrow: "<i class='slick-prev pull-left fas fa-angle-left' aria-hidden='true'></i>",
-    nextArrow: "<i class=' slick-next pull-right fas fa-angle-right' aria-hidden='true'></i>",
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-        {
-            breakpoint: 576,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: "<i class='slick-prev pull-left fas fa-angle-left' aria-hidden='true'></i>",
+        nextArrow: "<i class=' slick-next pull-right fas fa-angle-right' aria-hidden='true'></i>",
+        dots: false,
+        infinite: true,
+        autoplaySpeed: 2000,
+        autoplay: false,
+        responsive: [
+          {
+            breakpoint: 520,
             settings: {
-                slidesPerRow: 1,
-                rows: 1
+              slidesToShow: 1,
             }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesPerRow: 2,
-                rows: 1
-            }
-        },
-        {
-            breakpoint: 992,
-            settings: {
-                slidesPerRow: 3,
-                rows: 2
-            }
-        }
-    ]
+          }
+        ]
 });
 
 $('#toggle').click(function() {
